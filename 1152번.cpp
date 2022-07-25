7acd23e62699f198db010c968f8708cb3ba23fd4
prumy02 <69321810+prumy02@users.noreply.github.com>
2022-07-25 22:05:12 +0900

    Update 1152번.cpp

--- 1152번.cpp
+++ 1152번.cpp
@@ -1,24 +1,8 @@
 #include<iostream>
-#include<string>
-#include<cctype>
 using namespace std;
 
 int main() {
-	string Word;
-	getline(cin, Word);
-
-	int num = 1;
-
-	if (Word[0] == ' ') {
-		num = num - 1;
-	}
-
-	for (int i = 0; i < Word.length() - 1; i++) {
-		if (Word[i] == ' ') {
-			num++;
-		}
-	}
-
-	cout << num;
-	return 0;
+	int A, B;
+	cin >> A >> B;
+	cout << A+B;
 }
