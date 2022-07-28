8b71985cdd30872a473696a037fe1fe30a80267f
prumy02 <69321810+prumy02@users.noreply.github.com>
2022-07-28 21:59:42 +0900

    [07/28] 05. 함수

--- 문제풀이/문제풀이/소스.cpp
+++ 문제풀이/문제풀이/소스.cpp
@@ -1,28 +1,35 @@
 #include<iostream>
-#include<string>
+#include<vector>
 
 using namespace std;
+
+bool han(int n) {
+	if (n < 100) {
+		return true;
+	}
+	else {
+		if (n / 100 + n % 10 == 2*((n / 10) % 10)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+}
 
 int main() {
 
 	int N;
-	int cnt = 0;
-	int temp, num;
 	cin >> N;
 
-	num = N;
+	int cnt = 0;
 
-	while (1) {
-		temp = (N % 10) * 10 + (N / 10 + N % 10) % 10;
-		cnt++;
-		N = temp;
-
-		if (temp == num) {
-			break;
+	for (int i = 1; i < N+1; i++) {
+		if (han(i)) {
+			cnt++;
 		}
 	}
 
-	cout << cnt << "\n";
-
-	return 0;
+	cout << cnt;
 }
